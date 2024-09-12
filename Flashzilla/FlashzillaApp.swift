fef747cf9a451ea69825bcbd1a04b36443aadadb
aditya choubisa <adityachoubisa02@gmail.com>
2024-09-12 12:02:07 +0530

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by APPLE on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
